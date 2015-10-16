074ee000bc0373d378c6edbfd0074ecd2fd2720d
abroer <arjan.broer@gmail.com>
2015-10-16 19:52:15 +0200

    added the failsafe plugin for integration tests

--- src/test/java/nl/arjan/sandbox/RunCukesTest.java
+++ src/test/java/nl/arjan/sandbox/RunCukesTest.java
@@ -2,9 +2,19 @@ package nl.arjan.sandbox;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import junit.framework.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty"})
 public class RunCukesTest {
+
+    @Test
+    public void mustfail() {
+        boolean expected = true;
+
+        boolean actual = false;
+        Assert.assertEquals(expected, actual);
+    }
 }
